fa926999430cf55a8a4360a94ed5e5e485dee7fa
Matthew Hirst <matt@devicemagic.com>
2014-03-27 15:16:47 +0200

    Artist fetching from musicbrainz db working.

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+ssh: ssh -L 5433:localhost:5432 -N matt@188.226.205.151
 web: bundle exec rails s -p $PORT
--- app/models/musicbrainz/artist.rb
+++ app/models/musicbrainz/artist.rb
@@ -2,4 +2,7 @@ class Musicbrainz::Artist < Musicbrainz
   include Tire::Model::Search
   include Tire::Model::Callbacks
   include Indexing::ArtistIndex
+
+  self.table_name = 'artist'
+  self.inheritance_column = nil
 end
